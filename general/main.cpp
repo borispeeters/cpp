14b58a0b68385ec66587ca263fbeb8fa6af3c024
borispeeters <borispeeters.bp@gmail.com>
2020-07-28 14:50:50 +0200

    stl

--- general/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "test.h"
- 
-int main()
-{ 
-    std::cout << "Hello world!\n";
-    return 0;
-}
--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,11 +6,26 @@
 /*   By: bpeeters <bpeeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/11 17:00:28 by bpeeters      #+#    #+#                 */
-/*   Updated: 2020/07/11 17:00:47 by bpeeters      ########   odam.nl         */
+/*   Updated: 2020/07/28 09:57:58 by bpeeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
+
+# include <algorithm>
+# include <exception>
+
+template <typename T>
+typename T::iterator	easyfind(T & haystack, int needle)
+{
+	typename T::iterator	it;
+
+	it = std::find(haystack.begin(), haystack.end(), needle);
+
+	if (it == haystack.end())
+		throw std::exception();
+	return it;
+}
 
 #endif
